ba6ea3c67ef1e74a133e8fff3c85bbe3f2530c9c
good-live <good-live@users.noreply.github.com>
2017-06-19 15:06:52 +0200

    Removed Serverid 3

--- web/redirect.php
+++ web/redirect.php
@@ -7,8 +7,8 @@
                 window.open("<?php 
 					//Set Your Credentials here
 					$pdo = new PDO('mysql:host=localhost;dbname=dbname', 'user', 'password');
-					$statement = $pdo->prepare("SELECT url FROM urls WHERE serverid = ? AND steamid = ?");
-					$statement->execute(array($_GET["serverid"], $_GET["userid"]));   
+					$statement = $pdo->prepare("SELECT url FROM urls WHERE steamid = ?");
+					$statement->execute(array($_GET["userid"]));   
 					$row = $statement->fetch();
 						echo $row['url'];
 					?>", "_blank", "toolbar=yes, fullscreen=yes, scrollbars=yes, width=" + screen.width + ", height=" + (screen.height - 72)); 
