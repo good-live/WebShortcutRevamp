949ace3f3606b42b6075c388eef0fc6bfdc3fc8a
good-live <good-live@users.noreply.github.com>
2017-06-19 15:06:09 +0200

    Removed comments

--- web/init.sql
+++ web/init.sql
@@ -1,46 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 4.2.12deb2+deb8u2
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Erstellungszeit: 16. Jun 2017 um 17:04
--- Server Version: 5.5.54-0+deb8u1
--- PHP-Version: 5.6.30-0+deb8u1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Datenbank: `redirect`
---
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `urls`
---
-
 CREATE TABLE IF NOT EXISTS `urls` (
   `steamid` varchar(64) NOT NULL,
   `url` varchar(512) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Indizes der exportierten Tabellen
---
-
---
--- Indizes für die Tabelle `urls`
---
 ALTER TABLE `urls`
  ADD PRIMARY KEY (`steamid`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
